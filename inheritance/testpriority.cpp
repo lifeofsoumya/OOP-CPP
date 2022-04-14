ad8f5df205d1c8acbe3178be77f3d5e5b6da5c97
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-04-14 12:26:15 +0530

    test priority for inheritance

---
+++ inheritance/testpriority.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+class base1{
+public:
+    base1 ()
+    {cout << " constructor of class base1\n";}
+
+    ~base1 ()
+    {cout << " destructor of class base1\n";
+}
+};
+
+class base2{
+public:
+    base2(){cout << " constructor of class base2\n";}
+    ~base2()
+    {cout << " destructor of class base2\n";}
+
+};
+
+class derive1 : public base1, public base2{   
+    public:
+        derive1(){
+    cout << " constructor of class derive1\n";}
+
+    ~derive1()
+    {cout << " destructor of class derive1\n";}
+
+};
+
+
+
+
+main ()
+
+{
+    derive1 x;
+
+    cout << " Destructors are: "<<endl;
+
+}
