1d21db6be71589cb8d4144ad1bb7f20f53948659
Soumya Mondal <mondalsoumya2001@gmail.com>
2022-04-26 23:09:59 +0530

    ptr vs ref cpp added

---
+++ Polymorphism/PtrVsReference.cpp
@@ -0,0 +1,16 @@
+// Pointers vs Referance explained
+
+#include <iostream>
+using namespace std;
+
+int main()  
+{  
+    int i;  
+    int &a=i;  
+
+    int k;  
+    int *p;  
+    p=&k;  
+
+    return 0 
+}
